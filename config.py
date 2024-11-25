2431fbf15ac81e5d26494501eb2d83fd49d6b2a5
Moolchandvansh <tomerwalboy@gmail.com>
2024-11-25 23:22:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,13 +11,13 @@ PASSWORD = "21C6gBn0UkogVCKu5tBJB0bn0y3EroLG"  # redis password
 
 PRIVATE_CHAT_ID = -1002418182458  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
-COOKIE = ""
-ADMINS = [1317173146]
+COOKIE = "ndus=YeJ7cKHteHui0vTyP6UQKvMH8j7Zn4aF438bau3M"
+ADMINS = [1849719371]
 
 
-BOT_USERNAME = "teraboxdown_bot"
+BOT_USERNAME = "Moolchand_Terabox_bot"
 
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
-FORCE_LINK = "@RoldexVerse"
+FORCE_LINK = "@OG_MOOLCHAND"
 
 PUBLIC_EARN_API = ""  # https://publicearn.com/api
